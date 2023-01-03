3a4ee33af2845265d63096fce34406a3622e1073
Stefan Mindegaard Lyager <stefan.mindegaard.lyager@consultant.lego.com>
2023-01-03 12:47:33 +0100

    Fetch data from Giphy

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import { Position, QuerySection } from "./QuerySection";
 
 export const App = () => {
-  const handleSearch = (
+  const handleSearch = async (
     text: string,
     imageText: string,
     position: Position
   ) => {
-    // TODO: Fetch data from Giphy
+    const response = await fetch(
+      `https://api.giphy.com/v1/stickers/search?q=${encodeURIComponent(
+        text
+      )}&limit=3&rating=g&api_key=1bkG7ky5cmw5SLyvNfElcR1iYVzs38Zq`
+    );
+
+    const json = await response.json();
+    console.log(json);
   };
 
   return <QuerySection search={handleSearch} />;
